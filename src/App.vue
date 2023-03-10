0c40c140a55b7a1e9ea9ead4941a117ecb0aae44
liminhua <minhua.li@yunmaizhineng.com>
2023-03-10 16:38:35 +0800

    fix: 全局添加处理时间和日期的 JavaScript 库

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+dayjs.locale('zh-cn')
 
 const locale = reactive(zhCN)
 </script>
